4a7cfe6c576c0b66ec2b507484c5153998fae02d
2030314490 <2030314490@qq.com>
2019-06-20 06:16:02 +0000

    change

--- app/Http/Controllers/Api/v1/PayController.php
+++ app/Http/Controllers/Api/v1/PayController.php
@@ -85,7 +85,7 @@ class PayController extends Controller
     public function alipayNotify(Request $request)
     {
         $banner = Banner::find(1);
-        $banner->title = $request;
+        $banner->title = $request->all();
         $banner->update();
 //        // 校验输入参数
 //        $data  = app('alipay')->verify();
